354d08d62027ecca8c32f6999ed3baf93618b535
Robin de Bruin <robin@headfwd.com>
2024-09-30 14:41:17 +0200

    express server

---
+++ server/app.js
@@ -0,0 +1,14 @@
+// This is would normally not be in a frontend repo, but for the sake of the simplicity it is here.
+
+import express from "express";
+
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
