5b1371197020b3ffe2a4e4fa8576d12a83bcaf9e
Robin de Bruin <robin@headfwd.com>
2024-09-30 15:05:34 +0200

    add flights path to expressjs server

--- server/app.js
+++ server/app.js
@@ -1,12 +1,30 @@
 // This is would normally not be in a frontend repo, but for the sake of the simplicity it is here.
 
 import express from "express";
+import fs from "fs";
 
 const app = express();
 const port = 3000;
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
+});
+
+app.get("/flights", (req, res) => {
+  const filePath = "server/flights.json";
+
+  fs.readFile(filePath, "utf8", (err, data) => {
+    if (err) {
+      return res.status(500).json({ error: "Failed to read data" });
+    }
+
+    try {
+      const jsonData = JSON.parse(data);
+      res.json(jsonData);
+    } catch (parseError) {
+      res.status(500).json({ error: "Failed to parse JSON" });
+    }
+  });
 });
 
 app.listen(port, () => {
